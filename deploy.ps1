d8bce01d86851e4bbced7a5dee847db20ae36132
Lee Powell <lee.powell@hotmail.com>
2026-02-07 15:29:19 +0000

    menu update

--- deploy.ps1
+++ deploy.ps1
@@ -5,6 +5,7 @@ $ErrorActionPreference = "Stop"
 
 $S3_BUCKET = "leestechblog"
 $CF_DISTRIBUTION_ID = "E24YRPEGAHQC7D"
+$AWS_PROFILE = "blog"
 
 Write-Host "Building Hugo site..." -ForegroundColor Cyan
 hugo --minify
@@ -14,17 +15,19 @@ if ($LASTEXITCODE -ne 0) {
 }
 
 Write-Host "Syncing to S3 bucket: $S3_BUCKET..." -ForegroundColor Cyan
-aws s3 sync public/ "s3://$S3_BUCKET" --delete
+aws s3 sync public/ "s3://$S3_BUCKET" --profile $AWS_PROFILE --delete
 if ($LASTEXITCODE -ne 0) {
     Write-Host "S3 sync failed!" -ForegroundColor Red
     exit 1
 }
 
 Write-Host "Invalidating CloudFront cache..." -ForegroundColor Cyan
-aws cloudfront create-invalidation --distribution-id $CF_DISTRIBUTION_ID --paths "/*"
+aws cloudfront create-invalidation --distribution-id $CF_DISTRIBUTION_ID --paths "/*" --profile $AWS_PROFILE
 if ($LASTEXITCODE -ne 0) {
     Write-Host "CloudFront invalidation failed!" -ForegroundColor Red
     exit 1
 }
 
 Write-Host "Deployment complete!" -ForegroundColor Green
+Write-Host "Site URL: https://leestechblog.com" -ForegroundColor Cyan
+Write-Host "CloudFront URL: https://d3xxxxxx.cloudfront.net" -ForegroundColor Cyan
